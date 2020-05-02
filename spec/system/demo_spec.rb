65ff5801d78302fdf66f0c29152c93224397c736
Ben Sheldon <ben@codeforamerica.org>
2020-05-01 20:45:25 -0700

    Show demo banner when in demo mode or demo environment

---
+++ spec/system/demo_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'Deactivate Users', type: :system do
+  context 'when in demo environment' do
+    around do |example|
+      ClimateControl.modify DEMO_MODE: 'true' do
+        example.run
+      end
+    end
+
+    it 'shows the DEMO banner' do
+      visit root_path
+      expect(page).to have_content 'This site is for example purposes only'
+    end
+  end
+
+  context 'when NOT in demo environment' do
+    it 'does not show demo banner' do
+      visit root_path
+      expect(page).not_to have_content 'This site is for example purposes only'
+    end
+  end
+end
