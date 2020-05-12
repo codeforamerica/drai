93176de26a19568bd1ff73ce7ad82e78618cb92a
Anule Ndukwu <anule@codeforamerica.org>
2020-05-12 10:34:23 -0700

    Log out users after 15 minutes, per security agreement

--- spec/system/session_timeout_spec.rb
+++ spec/system/session_timeout_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 describe 'Session timeout' do
   let(:assister) { create :assister }
 
-  it 'logs person out after 20 minutes of inactivity' do
+  it 'logs person out after 15 minutes of inactivity' do
     sign_in assister
     visit root_path
 
-    Timecop.travel 19.minutes
+    Timecop.travel 14.minutes
     visit root_path
     expect(page).to have_link 'Sign out'
 
-    Timecop.travel 19.minutes
+    Timecop.travel 14.minutes
     visit root_path
     expect(page).to have_link 'Sign out'
 
-    Timecop.travel 21.minutes
+    Timecop.travel 16.minutes
     visit root_path
     expect(page).not_to have_link 'Sign out'
     expect(page).to have_content 'Your session expired. Please sign in again to continue.'
