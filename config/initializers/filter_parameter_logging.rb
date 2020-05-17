73fce116b00a895cba6c8d3e7abdc50ba9361798
Ben Sheldon <ben@codeforamerica.org>
2020-05-17 16:59:35 -0700

    FIlter PII fields from logs and Sentry

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,16 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [
+  /\Aname\z/,
+  /\Abirthday\z/,
+  /\Aphone_number\z/,
+  /\Aemail\z/,
+  /\Astreet_address\z/,
+  /\Aapartment_number\z/,
+  /\Amailing_street_address\z/,
+  /\Amailing_apartment_number\z/,
+  /\Acard_number\z/,
+  :password,
+  :password_confirmation,
+]
