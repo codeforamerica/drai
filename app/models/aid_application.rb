5f9756449fd55657344cb33a05a3293577be6af5
Whitney Schaefer <whitney.schaefer@gmail.com>
2020-05-18 11:54:58 -0700

    Revert "Revert "Deduplicate applications during approval""
    This reverts commit 438a3592ac0ef40c011786d42f2a574036cb9d5e.

--- app/models/aid_application.rb
+++ app/models/aid_application.rb
@@ -231,6 +231,10 @@ class AidApplication < ApplicationRecord
       )
   end
 
+  scope :matching_approved_apps, ->(aid_application) do
+    approved.matching_submitted_apps(aid_application)
+  end
+
   belongs_to :organization, counter_cache: true
   belongs_to :creator, class_name: 'User', inverse_of: :aid_applications_created, counter_cache: :aid_applications_created_count
   belongs_to :submitter, class_name: 'User', inverse_of: :aid_applications_submitted, counter_cache: :aid_applications_submitted_count, optional: :true
