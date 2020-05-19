addf02f61b6b5b4df1d0f7d35a1b6d9e0b7f4990
Whitney Schaefer <whitney.schaefer@gmail.com>
2020-05-19 13:55:45 -0700

    Wrap all routes in locale scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,42 @@
 Rails.application.routes.draw do
-  root 'hello_worlds#show'
+  # All routes in this scope will be prefixed with /locale if an available locale is set. See default_url_options in
+  # application_controller.rb and http://guides.rubyonrails.org/i18n.html for more info on this approach.
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    root 'hello_worlds#show'
 
-  devise_for :users,
-             skip: [:registrations],
-             controllers: {
-               confirmations: 'account/confirmations',
-             },
-             path: 'account'
+    devise_for :users,
+               skip: [:registrations],
+               controllers: {
+                 confirmations: 'account/confirmations',
+               },
+               path: 'account'
 
-  resource 'account', module: :account, only: [:show, :edit, :update] do
-    resource 'setup', only: [:edit, :update], path_names: { edit: '' }
-  end
+    resource 'account', module: :account, only: [:show, :edit, :update] do
+      resource 'setup', only: [:edit, :update], path_names: { edit: '' }
+    end
 
-  resource :privacy, only: [:show]
+    resource :privacy, only: [:show]
 
-  namespace :admin do
-    resources :organizations, only: :index
-    resources :users, only: :index
-    resources :aid_applications, only: :index
-  end
-
-
-  resources :organizations, only: [] do
-    scope module: :organizations do
-      resource :dashboard, only: [:show]
-      resources :assisters, except: [:destroy] do
-        member do
-          delete 'deactivate'
-          post 'reactivate'
-          post 'resend_confirmation_instructions'
-        end
-      end
-      resources :aid_applications, only: [:create, :destroy]
+    namespace :admin do
+      resources :organizations, only: :index
+      resources :users, only: :index
+      resources :aid_applications, only: :index
     end
 
 
-    # All routes in this scope will be prefixed with /locale if an available locale is set. See default_url_options in
-    # application_controller.rb and http://guides.rubyonrails.org/i18n.html for more info on this approach.
-    scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    resources :organizations, only: [] do
+      scope module: :organizations do
+        resource :dashboard, only: [:show]
+        resources :assisters, except: [:destroy] do
+          member do
+            delete 'deactivate'
+            post 'reactivate'
+            post 'resend_confirmation_instructions'
+          end
+        end
+        resources :aid_applications, only: [:create, :destroy]
+      end
+
       resources :aid_applications, only: [] do
         scope module: :aid_applications do
           resource :eligibility, only: [:edit, :update], path_names: { edit: '' }
