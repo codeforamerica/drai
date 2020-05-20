8aa854c48cd62ba21316ff5729fda33e18b647b5
Whitney Schaefer <whitney.schaefer@gmail.com>
2020-05-20 15:45:03 -0700

    Add kaminari and basic pagination for apps on org dashboard

--- app/controllers/organizations/dashboards_controller.rb
+++ app/controllers/organizations/dashboards_controller.rb
@@ -1,13 +1,11 @@
 module Organizations
   class DashboardsController < BaseController
-    LIMIT = 200
-
     def show
       aid_applications_query = current_organization.aid_applications
                                                    .includes(:organization, :creator, :submitter, :approver, :disburser)
                                                    .submitted
+                                                   .page(params[:page])
                                                    .order(id: :desc)
-                                                   .limit(LIMIT)
       aid_applications_query = aid_applications_query.query(params[:term]) if params[:term].present?
 
       @aid_applications = aid_applications_query
